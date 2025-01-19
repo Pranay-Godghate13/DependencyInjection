aafa146c6e83322911e22ada100685a8b9da300c
Pranay <pranaygodghate13@gmail.com>
2025-01-19 18:33:50 +0530

    Property value added inside bean

--- src/main/java/com/beandemo/beandemo/Main.java
+++ src/main/java/com/beandemo/beandemo/Main.java
@@ -6,7 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("Spring.xml");
-        Vehicle obj=(Vehicle)context.getBean("vehicle");
-        obj.drive();
+        // Vehicle obj=(Vehicle)context.getBean("bike");
+        // obj.drive();
+        // Tyre obj=new Tyre();
+        // obj.setBrand("CEAT");
+        Tyre obj=(Tyre)context.getBean("tyre");
+        System.out.println(obj);
     }
 }
